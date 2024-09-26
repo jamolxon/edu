8bf380bbc7e4f7d211e96dc57294debda7428f15
kozimov_01 <bekzodnodirbekovich@gmail.com>
2024-09-26 18:41:10 +0500

    feat: groups students

--- common/group/urls.py
+++ common/group/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from common.group.views import GroupCreateView, GroupDeleteView, GroupListView, GroupUpdateView
+from common.group.views import GroupCreateView, GroupDeleteView, GroupListView, GroupUpdateView, GroupStudentListView
 
 app_name = "group"
 
 urlpatterns = [
         path("", login_required(GroupListView.as_view()), name="group-list"), 
+        path("student/<int:group_id>/", login_required(GroupStudentListView.as_view()), name="group-student-list"), 
         path("<int:pk>/", login_required(GroupUpdateView.as_view()), name="group-update"), 
         path("create/", login_required(GroupCreateView.as_view()), name="group-create"), 
         path("delete/<int:pk>/", login_required(GroupDeleteView.as_view()), name="group-delete"), 
--- common/group/views.py
+++ common/group/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from helpers.views import CreateView, DeleteView, UpdateView
-from common.models import Group
+from common.models import Group, Student
 from common.group.forms import GroupForm
 
 
@@ -12,7 +12,7 @@ class GroupListView(ListView):
     model = Group
     template_name = "group/list.html"
     context_object_name = "objects"
-    queryset = model.objects.all().order_by("-id")
+    queryset = model.objects.all().order_by("-id").filter()
     paginate_by = 10
 
 
@@ -24,6 +24,21 @@ class GroupListView(ListView):
                     Q(name__icontains=search))
 
         return object_list
+    
+
+class GroupStudentListView(ListView):
+    model = Group
+    template_name = "group/detail.html"
+    context_object_name = "objects"
+    queryset = model.objects.all().order_by("-id")
+    paginate_by = 10
+
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['students'] =  Student.objects.filter(group_id=self.kwargs["group_id"])
+        print(context["students"])
+        return context
 
 
 class GroupCreateView(CreateView):
